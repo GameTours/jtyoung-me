89dbf96a377562075e4890b9b0b79805265f135f
GameTours <joe.young@sky.com>
2024-08-30 18:46:11 +0100

    arrange files
    first commit by me - moved files around

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'jtyoung.me';
+export const SITE_DESCRIPTION = 'The website for Joe Young - hosted on jtyoung.me';
