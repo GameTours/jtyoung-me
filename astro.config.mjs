1264470d058e4419c7d83c8650ddd58d4788c870
GameTours <joe.young@sky.com>
2026-03-14 17:46:02 +0000

    Changing host provider...

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  adapter: cloudflare(),
 });
